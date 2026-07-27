506f0b4f2a95840701ad0b717dad6a71989ec5ae
regina <risewins@gmail.com>
2026-07-27 14:50:33 +0300

    "Добавлена функция sort_by_date"

--- src/processing.py
+++ src/processing.py
@@ -13,3 +13,17 @@ def filter_by_state(list_of_dicts: list, state: str = 'EXECUTED') -> list:
         if item.get('state') == state:
             filtered_list.append(item)
     return filtered_list
+
+
+def sort_by_date(list_of_dicts: list, reverse_order: bool = True) -> list:
+    """
+    Сортирует список словарей по дате
+
+    Аргументы:
+        list_of_dicts: список словарей с данными операций
+        reverse_order: порядок сортировки (True - убывание, False - возрастание)
+
+    Возвращает:
+        Новый отсортированный список
+    """
+    return sorted(list_of_dicts, key=lambda x: x['date'], reverse=reverse_order)
