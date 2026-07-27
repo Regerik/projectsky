1a47ca79f4bd0ef937dcdad82461df98be20b617
regina <risewins@gmail.com>
2026-07-27 14:45:53 +0300

    "Добавлена функция filter_by_state"

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,6 @@
 def filter_by_state(list_of_dicts: list, state: str = 'EXECUTED') -> list:
     """
     Фильтрует список словарей по значению ключа 'state'
-
     Аргументы:
         list_of_dicts: список словарей с данными операций
         state: значение для фильтрации (по умолчанию 'EXECUTED')
